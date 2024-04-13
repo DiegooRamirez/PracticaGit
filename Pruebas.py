6c9959c1638020e8abab0cfb5c6837a65d18ae1c
Diego Ramirez <diego.emilio.raga@gmail.com>
2024-04-12 18:38:29 -0600

    Prueba de la segunda funcion OK

--- Pruebas.py
+++ Pruebas.py
@@ -1,3 +1,8 @@
 import Ejercicio1
-numero=int(input("Ingrese el numero de la tabla: "))
-Ejercicio1.tabla(numero)
+op=int(input("1. Tabla \n2. Area de un Cuadrado \n3. Operaciones \n¿Que funcion desea utilizar?: "))
+if op==1:
+    numero=int(input("Ingrese el numero de la tabla: "))
+    Ejercicio1.tabla(numero)
+if op==2:
+    lado=int(input("Ingrese el lado del cuadrado: "))
+    Ejercicio1.area_cuadrado(lado)
