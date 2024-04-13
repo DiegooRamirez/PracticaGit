4f1dc3d67bc1bd2dc8bcad1d8752758f44ab760a
Diego Emilio Ramirez Garcia <diego.emilio.raga@gmail.com>
2024-04-12 19:03:33 -0600

    Prueba tercera funcion OK

--- Pruebas.py
+++ Pruebas.py
@@ -3,6 +3,13 @@ op=int(input("1. Tabla \n2. Area de un Cuadrado \n3. Operaciones \n¿Que funcion
 if op==1:
     numero=int(input("Ingrese el numero de la tabla: "))
     Ejercicio1.tabla(numero)
-if op==2:
+
+elif op==2:
     lado=int(input("Ingrese el lado del cuadrado: "))
     Ejercicio1.area_cuadrado(lado)
+
+elif op==3:
+    opcion=int(input("1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n¿Qué desea hacer?: "))
+    num1=float(input("Ingrese el primer número: "))
+    num2=float(input("Ingrese el segundo número: "))
+    Ejercicio1.operaciones(opcion, num1, num2)
